e121463cf4f95a1b1ca99a5e2f0a5541ace519a4
valebearzotti <valebearzotti1@gmail.com>
2025-08-19 13:20:42 -0300

    add showcase to sitemap

--- apps/website/src/app/sitemap.ts
+++ apps/website/src/app/sitemap.ts
@@ -1,7 +1,7 @@
 export const baseUrl = "https://xmcp.dev";
 
 export default async function sitemap() {
-  const routes = ["", "/docs", "/x"].map((route) => ({
+  const routes = ["", "/docs", "/x", "/showcase"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
