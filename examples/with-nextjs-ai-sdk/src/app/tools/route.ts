a7c0899bdc42602aa06f7d801c559ba2380fc60e
joe <jlachance1@gmail.com>
2025-09-09 17:30:24 -0500

    Removes console.log

--- examples/with-nextjs-ai-sdk/src/app/tools/route.ts
+++ examples/with-nextjs-ai-sdk/src/app/tools/route.ts
@@ -2,8 +2,5 @@ import { toolRegistry } from "@xmcp/tools";
 
 export async function GET() {
   const response = await toolRegistry();
-
-  console.log(response);
-
   return Response.json(response);
 }
