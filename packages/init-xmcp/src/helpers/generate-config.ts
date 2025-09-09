93317733d83dde256bdc5742ffac5170a21622f0
joe <jlachance1@gmail.com>
2025-09-09 18:16:12 -0500

    Fixes generate-config in init-xmcp to only output a single path key.

--- packages/init-xmcp/src/helpers/generate-config.ts
+++ packages/init-xmcp/src/helpers/generate-config.ts
@@ -21,31 +21,24 @@ const config: XmcpConfig = {
     adapter: "${frameworkAdapter}",
   },`;
 
-  if (toolsPath) {
+  if (toolsPath || promptsPath) {
     configContent += `
-  paths: {
-    tools: "${toolsPath}",
-  },`;
-  }
-  if (promptsPath) {
-    configContent += `
-  paths: {
-    prompts: "${promptsPath}",
-  },`;
-  }
+  paths: {`;
 
-  if (toolsPath && promptsPath) {
-    configContent += `
-  paths: {
-    tools: "${toolsPath}",
-    prompts: "${promptsPath}",
-  },`;
-  }
+    if (toolsPath) {
+      configContent += `
+    tools: "${toolsPath}",`;
+    }
 
-  configContent += `
-};
+    if (promptsPath) {
+      configContent += `
+    prompts: "${promptsPath}",`;
+    }
+    configContent += `
+},;
 
 export default config;`;
+  }
 
   const configPath = path.join(projectPath, "xmcp.config.ts");
   fs.writeFileSync(configPath, configContent);
