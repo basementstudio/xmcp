3de1c5e582cb516c1779ea75e1706dd81e48fd0b
valebearzotti <valebearzotti1@gmail.com>
2025-09-16 11:30:39 -0300

    resources examples nit

--- examples/http-transport/src/resources/(config)/app.ts
+++ examples/http-transport/src/resources/(config)/app.ts
@@ -1,9 +1,5 @@
 import { type ResourceMetadata } from "xmcp";
 
-// uri comes from folder structure, resulting in config://app
-// no params - no schema
-
-// resulting URI is config://app
 export const metadata: ResourceMetadata = {
   name: "app-config",
   title: "Application Config",
