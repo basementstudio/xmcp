03df9f3c9a6e44ac363de87f18c6417af39db7f6
Valentina Bearzotti <64658886+valebearzotti@users.noreply.github.com>
2025-09-22 13:03:30 -0300

    titles consistency + overall polish (#168)

--- apps/website/src/components/showcase/cards/index.tsx
+++ apps/website/src/components/showcase/cards/index.tsx
@@ -38,8 +38,6 @@ export async function ShowcaseCard({
 } & ShowcaseItem) {
   const { name, tagline, repositoryUrl, logo, connection, tag } = item;
 
-  //const stars = repositoryUrl ? await getRepoStars(repositoryUrl) : null;
-
   return (
     <div
       className={cn(
@@ -72,7 +70,7 @@ export async function ShowcaseCard({
                   href={repositoryUrl}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="italic text-white font-medium hover:underline"
+                  className="text-white font-medium uppercase hover:underline"
                 >
                   {name}{" "}
                   <span className="invisible group-hover:visible text-sm">
@@ -80,7 +78,7 @@ export async function ShowcaseCard({
                   </span>
                 </Link>
               ) : (
-                <h3 className="italic text-white font-medium">{name}</h3>
+                <h4 className="text-white font-medium uppercase">{name}</h4>
               )}
             </div>
           </div>
@@ -110,18 +108,6 @@ export async function ShowcaseCard({
               </span>
             </div>
             <div className="flex items-center gap-2">
-              {/* {stars && (
-                <div className="flex items-center gap-1 text-xs text-[#BABABA]">
-                  <svg
-                    className="w-3 h-3"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                  </svg>
-                  {stars}
-                </div>
-              )} */}
               {repositoryUrl && (
                 <Link
                   href={repositoryUrl}
