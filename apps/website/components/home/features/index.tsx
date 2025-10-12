5da59fd709dceeb12302c49ff980f2eb0d0311ff
valebearzotti <valebearzotti1@gmail.com>
2025-10-12 17:29:00 -0300

    nit responsive

--- apps/website/components/home/features/index.tsx
+++ apps/website/components/home/features/index.tsx
@@ -11,7 +11,7 @@ import { Icons } from "@/components/icons";
 export const Features = () => {
   return (
     <div className="col-span-12 grid grid-cols-12 gap-[20px]">
-      <div className="flex flex-col items-start justify-center col-span-9 col-start-2 w-full mx-auto mb-8 gap-4">
+      <div className="flex flex-col items-start justify-center col-span-12 md:col-span-9 md:col-start-2 w-full mx-auto mb-8 gap-4">
         <Tag text="Features" />
         <div className="flex flex-col md:flex-row items-start md:items-center justify-center gap-4 md:gap-8 w-full">
           <h2 className="heading-2 text-balance">
