7681dff7893c3dc26b9eb2a3b732c7d4dd195f64
valebearzotti <valebearzotti1@gmail.com>
2025-10-13 09:32:16 -0300

    prefooter

---
+++ apps/website/components/layout/prefooter/index.tsx
@@ -0,0 +1,13 @@
+import BgImg from "./x.png";
+import Image from "next/image";
+
+export const Prefooter = () => {
+  return (
+    <div className="relative flex flex-col justify-center items-center min-h-[300px] md:min-h-[600px]">
+      <Image src={BgImg} alt="Prefooter" width={127} height={154} />
+      <h3 className="text-2xl font-normal leading-10 tracking-[-0.03rem] -translate-y-4">
+        One framework to rule them all
+      </h3>
+    </div>
+  );
+};
