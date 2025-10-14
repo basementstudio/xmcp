e037da4728f91d7957b7809fdebe28043e886700
valebearzotti <valebearzotti1@gmail.com>
2025-10-14 09:52:22 -0300

    remove old home sections + stuff related to the x logo and shader

--- apps/website/components/terminal/logo/index.tsx
+++
@@ -1,8 +0,0 @@
-import { fetchAssets } from "@/basehub/";
-import { XmcpLogo as ThreeLogo } from "./client";
-
-export const XmcpLogo = async () => {
-  const assets = await fetchAssets();
-
-  return <ThreeLogo matcap={assets.glLogoMatcap.url} />;
-};
