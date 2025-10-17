93894edfac2c0adfd1c82ee6049fcc7a93370716
valebearzotti <valebearzotti1@gmail.com>
2025-10-17 15:39:09 -0700

    fix responsive

--- apps/website/components/home/shader.tsx
+++ apps/website/components/home/shader.tsx
@@ -4,7 +4,7 @@ import ParticlesCursorAnimation from "./particles-cursor-animation";
 
 export default function Shader() {
   return (
-    <main className="w-full h-[500px]">
+    <main className="w-full aspect-square max-h-[250px] sm:max-h-[350px] md:max-h-[450px] lg:max-h-[500px]">
       <Canvas
         camera={{ position: [0, 0, 18], fov: 35 }}
         gl={{ antialias: true }}
