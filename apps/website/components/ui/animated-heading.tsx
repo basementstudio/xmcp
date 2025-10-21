c6d3131f898dce8cc89912edde0d0a04a78fcbe8
Fede Álvarez <fedealvarezcampos@gmail.com>
2025-10-21 02:26:45 +0200

    reveal

--- apps/website/components/ui/animated-heading.tsx
+++ apps/website/components/ui/animated-heading.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { cn } from "@/utils/cn";
+
+gsap.registerPlugin(ScrollTrigger);
 
 interface AnimatedHeadingProps {
   children: React.ReactNode;
@@ -23,10 +27,19 @@ export function AnimatedHeading({
 }: AnimatedHeadingProps) {
   const textRef = useRef<HTMLHeadingElement>(null);
 
-  useEffect(() => {
+  useGSAP(() => {
     if (!textRef.current) return;
 
     const element = textRef.current;
+
+    const initTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        start: "top 80%",
+        end: "top 60%",
+        toggleActions: "play none none reverse",
+      },
+    });
 
     const timeline = gsap.timeline({
       delay,
@@ -34,21 +47,62 @@ export function AnimatedHeading({
       ease: "none",
     });
 
-    timeline.to(element, {
-      backgroundPosition: "250% 0%",
-      duration: duration,
-      ease: "none",
+    initTl.set(element, {
+      filter: "blur(14px)",
+      maskImage: "radial-gradient(circle at center, black 0%, transparent 0%)",
+      maskRepeat: "no-repeat",
+      maskPosition: "center center",
+      WebkitMaskImage:
+        "radial-gradient(circle at center, black 0%, transparent 0%)",
     });
 
-    return () => {
-      timeline.kill();
-    };
+    // mask
+    initTl.to(element, {
+      autoAlpha: 1,
+      filter: "blur(0px)",
+      duration: 1,
+      ease: "power2.out",
+    });
+
+    initTl.to(
+      element,
+      {
+        maskImage:
+          "radial-gradient(circle at center, black 60%, transparent 200%)",
+        WebkitMaskImage:
+          "radial-gradient(circle at center, black 60%, transparent 200%)",
+        duration: 3,
+        ease: "power2.out",
+      },
+      "<"
+    );
+
+    initTl.set(
+      element,
+      {
+        filter: "none",
+        maskImage: "none",
+        WebkitMaskImage: "none",
+      },
+      ">"
+    );
+
+    // continued bg anim
+    timeline.to(
+      element,
+      {
+        backgroundPosition: "250% 0%",
+        duration: duration,
+        ease: "none",
+      },
+      2
+    );
   }, [delay, duration]);
 
   return (
     <Component
       ref={textRef}
-      className={cn("relative text-gradient", className)}
+      className={cn("relative text-gradient bg-clip-text", className)}
     >
       {children}
     </Component>
