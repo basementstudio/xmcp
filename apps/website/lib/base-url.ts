84157975f1403659ec691c5165527bb78ebef1f4
0xKoller <joseluiskoller98@gmail.com>
2025-12-12 13:15:14 -0300

    base url

--- apps/website/lib/base-url.ts
+++ apps/website/lib/base-url.ts
@@ -1,7 +1,4 @@
 export function getBaseUrl(): string {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
 
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:3000";
