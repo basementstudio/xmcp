d4b25cb34df05d466922c15dbe6d3221846cea38
valebearzotti <valebearzotti1@gmail.com>
2025-12-12 14:12:36 -0300

    og meta card

--- apps/website/app/blog/[...slug]/layout.tsx
+++ apps/website/app/blog/[...slug]/layout.tsx
@@ -1,6 +1,19 @@
+import type { Metadata } from "next";
 import { BlogLayout } from "@/components/layout/blog";
 import { baseOptions } from "@/lib/layout.shared";
 import { blogSource } from "@/lib/source";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://xmcp.dev"),
+  openGraph: {
+    siteName: "xmcp",
+    type: "website",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
+};
 
 export default function Layout({ children }: LayoutProps<"/blog/[...slug]">) {
   return (
