cc8fc33e486ae280fbd083d22e4fa206bcf3c3b6
0xKoller <joseluiskoller98@gmail.com>
2026-01-22 15:20:22 -0300

    delete stuff

--- packages/xmcp/src/runtime/adapters/nestjs/index.ts
+++ packages/xmcp/src/runtime/adapters/nestjs/index.ts
@@ -1,21 +1,3 @@
 export { XmcpModule } from "./xmcp.module";
-export { XmcpCoreModule } from "./xmcp-core.module";
 export { XmcpService } from "./xmcp.service";
 export { XmcpController } from "./xmcp.controller";
-
-// Auth exports
-export {
-  XmcpAuthGuard,
-  XmcpAuth,
-  Auth,
-  McpAuth,
-  AUTH_CONFIG_KEY,
-  ResourceMetadataController,
-  AUTHORIZATION_SERVERS,
-} from "./auth";
-export type {
-  AuthConfig,
-  AuthInfo,
-  VerifyToken,
-  OAuthProtectedResourceMetadata,
-} from "./auth";
