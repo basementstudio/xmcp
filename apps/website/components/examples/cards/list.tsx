91a14264a6aa693009d1845c6bfd6a4b53097cec
0xKoller <joseluiskoller98@gmail.com>
2026-04-17 15:29:51 -0300

    rename

--- apps/website/components/examples/cards/list.tsx
+++
@@ -1,8 +0,0 @@
-import { fetchExamplesAndTemplates } from "@/app/examples/utils/github";
-import { ExampleCards } from "./cards";
-
-export async function ExampleCardsList() {
-  const examples = await fetchExamplesAndTemplates();
-
-  return <ExampleCards examples={examples} searchTerm="" />;
-}
