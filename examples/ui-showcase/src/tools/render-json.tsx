4a2ee6aefbec289fa1fa3073331271cf448c3b89
0xKoller <joseluiskoller98@gmail.com>
2026-04-21 16:45:35 -0300

    update mcp app fix

--- examples/ui-showcase/src/tools/render-json.tsx
+++
@@ -1,9 +0,0 @@
-import { createRenderJsonTool } from "@xmcp-dev/ui";
-
-const renderJsonTool = createRenderJsonTool({
-  transportMode: "host",
-});
-
-export const metadata = renderJsonTool.metadata;
-export const schema = renderJsonTool.schema;
-export default renderJsonTool.handler;
